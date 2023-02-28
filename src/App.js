eac28044f54a54e82171650dc378f208a628670e
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-28 17:26:14 +0100

    Iteración 1done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import contacts from "./contacts.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const onlyFiveContacts = contacts.slice(0, 5);
+  console.log(onlyFiveContacts);
+
+  return(
+  <div className="App">
+    <div>
+      <h1>IronContacts</h1>
+      <table class="tabla-contactos">
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {onlyFiveContacts.map((eachElement) => {
+          return (
+            <tr>
+              <td><img src={eachElement.pictureUrl} alt={eachElement.name} width="100px" /></td>
+              <td>{eachElement.name}</td>
+              <td>{eachElement.popularity.toFixed(2)}</td>
+            </tr>
+          )
+        })}
+      </table>
     </div>
-  );
+
+
+
+  </div>
+  )
 }
 
 export default App;
