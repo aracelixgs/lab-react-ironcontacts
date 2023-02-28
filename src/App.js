f8bde1718795cb1e3a5633de0ccb267149d03855
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-28 18:16:30 +0100

    Iteración 3 done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,31 @@ import contacts from "./contacts.json";
 function App() {
 
   const onlyFiveContacts = contacts.slice(0, 5);
-  console.log(onlyFiveContacts);
+  //console.log(onlyFiveContacts);
+
+  const [ actualContacts, setNewArr ] = useState(onlyFiveContacts)
+
+  const addRandomContact = () => {
+    const randomNumber = Math.random() * contacts.length;
+    const randomPosition = Math.floor(randomNumber);
+    const randomContact = contacts[randomPosition];
+    //console.log(randomContact)
+    const cloneContacts = [...actualContacts]
+    console.log(cloneContacts)
+    cloneContacts.unshift(randomContact)
+    setNewArr( cloneContacts )
+  }
+
 
   return(
   <div className="App">
     <div>
       <h1>IronContacts</h1>
-      <table class="tabla-contactos">
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
+
+      <table className="tabla-contactos">
+        <thead>
         <tr>
           <th>Picture</th>
           <th>Name</th>
@@ -19,8 +37,10 @@ function App() {
           <th>Won an Oscar</th>
           <th>Won an Emmy</th>
         </tr>
-        {onlyFiveContacts.map((eachElement) => {
+        </thead>
+        {actualContacts.map((eachElement) => {
           return (
+            <tbody key={eachElement.id}>
             <tr>
               <td><img src={eachElement.pictureUrl} alt={eachElement.name} width="100px" /></td>
               <td>{eachElement.name}</td>
@@ -28,6 +48,7 @@ function App() {
               <td>{eachElement.wonOscar === true && <h2>🏆</h2>}</td>
               <td>{eachElement.wonEmmy === true && <h2>🏆</h2>}</td>
             </tr>
+            </tbody>
           )
         })}
       </table>
